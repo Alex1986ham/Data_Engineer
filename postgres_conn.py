6eb3063b4b1650a5ad6c5bf2a17b91d6c4e80d75
Alex1986ham <34843315+Alex1986ham@users.noreply.github.com>
2019-05-13 16:40:49 +0200

    Update postgres_conn.py

--- postgres_conn.py
+++ postgres_conn.py
@@ -1,2 +1,46 @@
 import psycopg2
 
+# Create a connection to the database
+try:
+  conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+except psycopg2.Error as e:
+  print("Error: Could not make connection to the Pstgres database")
+  print(e)
+  
+# Getting a cursor
+try:
+  cur = conn.cursor()
+ except psycopg2.Error as e:
+    print("Error: Could not get cursor to the Database")
+    print(e)
+
+conn.set_session(autocomit=True)
+
+try;
+  cur.execute("create database orders")
+except psycopg2(Error as e:
+  print(e)
+
+try:
+  conn.close()
+except psycopg2.Error as e:
+  print(e)
+
+try:
+  conn = psycopg2.connect("host=127.0.0.1 dbname=orders user=student password=student")
+except psycopg2.Error as e:
+  print("Error: Could not make connection to the Postgres dabase")
+  print(e)
+  
+  
+cur.execute("select * from test")
+
+           
+try:                
+  cur.execute("CREATE TABLE test (col1 int, col2 int, col3 int);")
+except: psycopg2.Error as e:
+  print("Error: Issue creating table")
+  print(e)
+
+try:
+                cur.execute("INSERT INTO 
