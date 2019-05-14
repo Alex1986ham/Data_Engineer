0f6def436952e49647a3b45a5798f0f15e49de29
Alex1986ham <34843315+Alex1986ham@users.noreply.github.com>
2019-05-14 20:31:43 +0200

    Update apache_cass.py

--- apache_cass.py
+++ apache_cass.py
@@ -34,3 +34,39 @@ except Exception as e:
 # Create table
 query = "CREATE TABLE IF NOT EXISTS music_library "
 query = query + "(year int, artist_name text, album_name text, PRIMARY KEY (year, artist_name))"
+try:
+    session.execute(query)
+except Exception as e:
+    print(e)
+    
+    
+ # Insert rows
+query = "INSERT INTO music_library (year, artist_name, album_name)'
+query = query + " VALUES (%s, %s. %s)"
+
+try:
+    session.execute(query, (1970, "The Beatles", "Let it Be"))
+ except Exception as e:
+    print(e)
+
+try:
+    session.execute(query, (1965, "The Beatles", "Rubber Soul"))
+except Exception as e:
+    print(e)
+    
+    
+# Validate inserted data
+query = 'SELECT * FROM music_library'
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
+    
+for row in rows:
+    print (row.year, row.album_name, row.artist_name
+           
+query = "drop table music_library"
+try:
+           rows = session.execute(query)
+  except Exception as e: 
+           print(e)
