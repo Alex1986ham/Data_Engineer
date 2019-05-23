b0f6d26eb37413d3b25fa1c90cf7daba0683c5c8
Alex <alexander.dudko@eufh.de>
2019-05-23 22:56:34 +0200

    til select all running

--- Mysql_DB/conn.py
+++ Mysql_DB/conn.py
@@ -7,36 +7,42 @@ try:
                                         database='test',
                                         user='aldudko2',
                                         password='Dudko010914')
-    print("Woooorks")
+    print("1. Woooorks")
 except Error as e:
     print("Error while connection to mysql", e)
 
 
+
 try:
     cur = conn.cursor()
+    print("2. cursor ok")
 except Error as e:
     print("Error: Could not get curser to the Database")
     print(e)
 
+conn.autocommit=True
 
 
 # Create TABLE
 try:
-    cur.execute("CREATE TABLE IF NOT EXISTS music_libary (album_id int, \
-                                                        album_name varchar, artist_name varchar \
-                                                        year int, songs text []);")    
+    cur.execute("CREATE TABLE IF NOT EXISTS music_libary(album_id int(10) NOT NULL, album_name varchar(20) NULL, artist_name varchar(20) NULL, year int(5) NULL, songs text(30) NULL)")
+    print("3. table created")
 except Error as e:
     print("Error: Issue creating table")
     print(e)
+
 
 # Insert INTO
 try:
     cur.execute("INSERT INTO music_libary (album_id, album_name, artist_name, year, songs) \
                 VALUES (%s, %s, %s, %s, %s)", \
-                (1, "Rubber Soul", "The Beatles", 1965, ["Let it be", "Accross the universe"]))
+                (1, "Rubber Soul", "The Beatles", 1965, "Let it be"))
+    print("4. inserted")
 except Error as e:
     print("Error: Inserting rows")
     print(e)
+
+
 
 # confirm the data got inserted
 try:
@@ -48,4 +54,4 @@ except Error as e:
 row = cur.fetchone()
 while row:
     print(row)
-    row = cur.fetchone
+    #row = cur.fetchone
