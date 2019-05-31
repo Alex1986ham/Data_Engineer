5ef05b0761df98199c435a1128085e1718273e62
Alex <alexander.dudko@eufh.de>
2019-05-31 07:12:48 +0200

    ...

---
+++ NoSQL/exercise3.py
@@ -0,0 +1,50 @@
+import cassandra
+
+try:
+    cluster = Cluster(['192.168.178.22'])
+    session = cluster.connect()
+except Exception as e:
+    print(e)
+
+
+#create keyspace
+try:
+    session.execute("""CREATE KEYSPACE IF NOT EXISTS udacity
+                    WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1 }"""
+                    )
+except Exception as e:
+    print(e)
+
+#Connection
+try:
+    session.set_keyspace('udacity')
+except Exception as e:
+    print(e)
+
+#creating table
+query = "CREATE TABLE IF NOT EXISTS music_libary "
+query = query + "(year int, artist_name text, album_name text, city text, PRIMARY KEY (artist_name, album_name, city))"
+try:
+    session.execute(query)
+except Exception(query)
+    print(e)
+
+
+#insert data
+query = "INSERT INTO music_libary (year, artist_name, album_name, city)"
+query = query + " VALUES (%s, %s, %s, %s)"
+
+try:
+    session.execute(query, (1970, "The BEATLES", "Let it be", "Liverpool"))
+except Exception as e:
+    print(e)
+
+#query
+query = "select * from music_libary WHERE artist_name='The Beatles'"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
+
+for row in rows:
+    print(row.year, row.artist_name, row.album_name, row.city)
