60eb1c5b09d9c8e9479f155fcc1c2708ae83a1da
Alex <alexander.dudko@eufh.de>
2019-06-08 23:43:38 +0200

    tesg

--- Web_Forms/FlaskApp/app.py
+++ Web_Forms/FlaskApp/app.py
@@ -1,3 +1,5 @@
+# https://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972
+
 from flask import Flask, render_template, request, json
 app = Flask(__name__)
 
