e23bfa3262faadd76ac054871bf29d30232d1f46
Alex1986ham <34843315+Alex1986ham@users.noreply.github.com>
2019-07-18 07:19:16 +0200

    update
    rdfd

--- AWS_DWH/creating_redshift_cluster.py
+++ AWS_DWH/creating_redshift_cluster.py
@@ -29,7 +29,6 @@ pd.DataFrame({"Param":
                   [DWH_CLUSTER_TYPE, DWH_NUM_NODES, DWH_NODE_TYPE, DWH_CLUSTER_IDENTIFIER, DWH_DB, DWH_DB_USER, DWH_DB_PASSWORD, DWH_PORT, DWH_IAM_ROLE_NAME]
              })
 
-import boto3
 
 ec2 = boto3.resource('ec2',
                        region_name="us-west-2",
@@ -55,7 +54,8 @@ redshift = boto3.client('redshift',
                        )
 
 
-sampleDbBucket =  s3.Bucket("awssampledbuswest2")
+
+sampleDbBucket =  s3.Bucket("awssampledbuswest22")
 for obj in sampleDbBucket.objects.filter(Prefix="ssbgz"):
     print(obj)
 # for obj in sampleDbBucket.objects.all():
@@ -123,58 +123,3 @@ def prettyRedshiftProps(props):
 
 myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
 prettyRedshiftProps(myClusterProps)
-
-
-
-DWH_ENDPOINT = myClusterProps['Endpoint']['Address']
-DWH_ROLE_ARN = myClusterProps['IamRoles'][0]['IamRoleArn']
-endpoint = config.get("DWH","DWH_ENDPOINT")
-print("DWH_ENDPOINT :: ", endpoint)
-print("DWH_ROLE_ARN :: ", roleArn)
-
-
-
-try:
-    vpc = ec2.Vpc(id=myClusterProps['VpcId'])
-    defaultSg = list(vpc.security_groups.all())[0]
-    print(defaultSg)
-    defaultSg.authorize_ingress(
-        GroupName=defaultSg.group_name,
-        CidrIp='0.0.0.0/0',
-        IpProtocol='TCP',
-        FromPort=int(DWH_PORT),
-        ToPort=int(DWH_PORT)
-    )
-except Exception as e:
-    print(e)
-
-"""
-%load_ext sql
-"""
-
-conn_string="postgresql://{}:{}@{}:{}/{}".format(DWH_DB_USER, DWH_DB_PASSWORD, DWH_ENDPOINT, DWH_PORT,DWH_DB)
-print(conn_string)
-#%sql $conn_string
-
-
-"""
-#### CAREFUL!!
-#-- Uncomment & run to delete the created resources
-redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
-#### CAREFUL!!
-"""
-
-
-"""
-myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
-prettyRedshiftProps(myClusterProps)
-"""
-
-
-"""
-#### CAREFUL!!
-#-- Uncomment & run to delete the created resources
-iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
-iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
-#### CAREFUL!!
-"""
