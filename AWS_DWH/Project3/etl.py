0e17c4c89346a79d381e84674a516372a8306f90
Alexander Dudko <alexander.dudko@eufh.de>
2019-07-22 13:36:56 +0200

    version3

---
+++ AWS_DWH/Project3/etl.py
@@ -0,0 +1,37 @@
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries, insert_table_queries
+
+
+def load_staging_tables(cur, conn):
+    """Stage data from S3 into tables in Redshift."""
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def insert_tables(cur, conn):
+    """Transform data from staging tables into analytical tables in star schema."""
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    """Load staging tables from S3 then transform into analytical tables."""
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+    print("Start loading tables")
+    load_staging_tables(cur, conn)
+    print("Start insert into tables")
+    insert_tables(cur, conn)
+
+    conn.close()
+
+
+
+if __name__ == "__main__":
+    main()
